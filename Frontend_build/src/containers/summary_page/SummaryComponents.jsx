0c61cfa650c06a9846a9554262d529a8d4e2c28e
Phil123095 <68194774+Phil123095@users.noreply.github.com>
2022-05-14 21:03:50 +0200

    responsive design woohooo

--- Frontend_build/src/containers/summary_page/SummaryComponents.jsx
+++ Frontend_build/src/containers/summary_page/SummaryComponents.jsx
@@ -43,9 +43,9 @@ function LandingPage() {
     }
 
     return (
-    <div class="mt-28">
-        <div class="w-100 min-h-screen flex flex-row md:grid md:grid-cols-8 md:grid-rows-6 md:gap-4 md:mx-14">
-            <div class="md:col-span-4 md:col-start-1 md:row-span-3 bg-slate-500 justify-center items-center rounded-lg">
+    <div class="mt-4 md:mt-28">
+        <div class="w-100 min-h-screen grid grid-cols-1 md:grid-cols-8 md:grid-rows-6 md:gap-4 md:mx-14">
+            <div class="m-2 col-span-1 md:col-span-4 md:col-start-1 md:row-span-3 bg-slate-500 justify-center items-center rounded-lg">
                 <div class="p-2 bg-white rounded-lg border shadow-sm">
                     <div class="pt-1 ml-1 h-10 border-b-1 border-color border-blue-900 text-base text-gray-700 items-center font-light">
                         Drop in the text you want to summarize here...
@@ -63,7 +63,7 @@ function LandingPage() {
                     </div>
                 </div>
             </div>
-            <div class="p-2 md:col-span-4 md:row-span-3 bg-white justify-center items-center rounded-lg">
+            <div class="m-2 p-2 col-span-1 md:col-span-4 md:row-span-3 bg-white justify-center items-center rounded-lg">
                 <div class="pt-1 ml-1 h-10 border-b-1 border-color border-blue-900 text-base text-gray-700 items-center font-light">
                     ... and the magic happens here.
                 </div>
@@ -72,7 +72,7 @@ function LandingPage() {
                 </div>
 
             </div>
-            <div class="h-20 w-full md:col-span-4 border-slate-200 rounded-lg bg-slate-100 px-2">
+            <div class="h-20 col-span-1 md:col-span-4 border-slate-200 rounded-lg bg-slate-100 px-2">
                 <p class="pt-1 px-2 text-base font-light text-gray-700">Select the summary size (as % of total text length)</p>
                 <div class="py-1 px-2 mb-3 h-2">
                     <Slider
@@ -88,7 +88,7 @@ function LandingPage() {
                     />
                 </div>
             </div>
-            <div class="h-20 w-full md:col-span-4 border-slate-100 rounded-lg bg-slate-100 items-center">
+            <div class="m-2 mb-3 h-10 md:h-20 col-span-1 md:col-span-4 border-slate-100 rounded-lg bg-slate-100 items-center">
                 <Button variant="contained" disableElevation fullWidth style={{minHeight: '60px', maxHeight: '60px'}} onClick={SummarizeText}>
                     <p class="text-base">Summarize</p>
                 </Button>
