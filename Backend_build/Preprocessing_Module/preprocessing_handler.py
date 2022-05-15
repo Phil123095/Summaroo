5fbb198c1eb2e52390d0d35fd9fe3762afa90ab4
Phil123095 <68194774+Phil123095@users.noreply.github.com>
2022-05-15 14:16:27 +0200

    Skeleton of media preprocessing

---
+++ Backend_build/Preprocessing_Module/preprocessing_handler.py
@@ -0,0 +1,53 @@
+import json
+from Media_Class import Media
+import requests
+
+
+def recommend_model(media_content):
+    """
+    Just a base skeleton of what the model recommendation function would look like. So everything below is an idea,
+    not a final build.
+    :param media_content:
+    :return:
+    """
+
+    models = {"LexRank": "https://bq5g5pjjc6fbzptimanr6v2gqu0tyydw.lambda-url.eu-central-1.on.aws/"}
+
+    if media_content.media_format == "text":
+        model_to_use = "LexRank"
+        model_api_url = models[model_to_use]
+
+        decision_factor = "Filtering"
+        return decision_factor, model_to_use, model_api_url
+
+
+def request_summarization(media_content, url_to_request):
+
+    response = requests.post(url_to_request, data={"full_text": media_content.final_clean_text,
+                                                 "perc_length": media_content.reduction_perc})
+
+    content = json.loads(response.content.decode('utf-8'))
+
+    return content
+
+
+def lambda_handler(event, context):
+    message = json.loads(event['body'])
+
+    text_to_summarise = message['full_text']
+    percent_reduce = message['perc_length']
+
+    WorkingContent = Media(media=text_to_summarise, perc_reduction=percent_reduce, format="text")
+    WorkingContent.convert_and_clean_media()
+
+    decision_reason, model_recommendation, model_endpoint = recommend_model(media_content=WorkingContent)
+
+    WorkingContent.model_decision_factor = decision_reason
+    WorkingContent.model_to_use = model_recommendation
+
+    final_summary_out = request_summarization(media_content=WorkingContent, url_to_request=model_endpoint)
+
+    WorkingContent.final_summary = final_summary_out['final_summary']
+    WorkingContent.info_to_DB()
+
+    return final_summary_out
--- Backend_build/Summarization_Models/LexRank-v.1/summarizer.py
+++ Backend_build/Summarization_Models/LexRank-v.1/summarizer.py
@@ -17,54 +17,33 @@ def text_cleaner(full_text):
 
 
 def summarization(full_text, reduce_length):
-    print("SUMMARIZINH")
     clean_text = text_cleaner(full_text)
     full_text_length = len(clean_text)
-    print(full_text_length)
 
     summary_perc = int(reduce_length)/100
 
     final_sentence_count = full_text_length * summary_perc
-    print(final_sentence_count)
+
     rawList = [item for item in full_text.splitlines() if item.strip()]
 
     rawList_str = ' '.join(rawList)
-    print(rawList_str)
     parser = PlaintextParser.from_string(rawList_str, Tokenizer('english'))
     # Summarise
     Lex = LexRankSummarizer()
     Lex_summary = Lex(document=parser.document, sentences_count=round(final_sentence_count))
-    print(Lex_summary)
 
     final_summary = ' '.join(str(v) for v in Lex_summary)
 
-    print(final_summary)
     return final_summary
 
 
 def lambda_handler(event, context):
-    print("Message received")
+
     message = json.loads(event['body'])
     text_to_summarise = message['full_text']
     percent_reduce = message['perc_length']
 
-    print(f"Text to summarise: {text_to_summarise}")
-    print("Performing summarization")
     result_summary = summarization(full_text=text_to_summarise, reduce_length=percent_reduce)
-    print("Summary done")
-    print(result_summary)
-
-    """
-        return {
-            'statusCode': 200,
-            'headers': {
-                'Access-Control-Allow-Headers': 'Content-Type',
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Methods': 'OPTIONS,POST'
-            },
-            'body': {'final_summary': result_summary}
-        }
-    """
 
     return {'final_summary': result_summary}
 
