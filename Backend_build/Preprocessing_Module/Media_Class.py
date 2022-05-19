23d479b42c33e2de99ee905e56f41fd1ee4999e2
Phil123095 <68194774+Phil123095@users.noreply.github.com>
2022-05-19 13:15:04 +0200

    Building out Media Class & conversion processes. Testing modified LexRank.

--- Backend_build/Preprocessing_Module/Media_Class.py
+++ Backend_build/Preprocessing_Module/Media_Class.py
@@ -1,10 +1,16 @@
 import time
+from youtube_transcript_api import YouTubeTranscriptApi
+from urllib.parse import urlparse
+from urllib.parse import parse_qs
+from pdfminer.high_level import extract_pages
+from pdfminer.layout import LTTextContainer
 
 
 class Media:
     def __init__(self, media, perc_reduction, format=None):
         self.request_time = time.time()
         self.raw_media = media
+        self.raw_text = None
         self.reduction_perc = perc_reduction
 
         if format:
@@ -30,6 +36,7 @@ class Media:
         self.time_to_summarise = None
 
     def __determine_format(self):
+
         """
         In case the request does not include the media format, this should determine what format the media is in.
         E.g. .mp4 -> Video, str -> str, etc.
@@ -42,7 +49,64 @@ class Media:
         Based on format, run different steps to convert the media (E.g. PDF to text conversion, youtube to text, etc.)
         :return:
         """
-        pass
+        if self.media_format == "text":
+            self.__basic_text_handle()
+        elif self.media_format == "youtube":
+            self.__extract_YT_transcript()
+        elif self.media_format == "pdf":
+            self.__extract_PDF_text()
+        return
+
+    def __basic_text_handle(self):
+        self.raw_text = self.raw_media
+        return
+
+
+    def __get_vid_id(self):
+
+        query = urlparse(self.raw_media)
+        if query.hostname == 'youtu.be':
+            return query.path[1:]
+        if query.hostname in ('www.youtube.com', 'youtube.com'):
+            if query.path == '/watch':
+                p = parse_qs(query.query)
+                return p['v'][0]
+            if query.path[:7] == '/embed/':
+                return query.path.split('/')[2]
+            if query.path[:3] == '/v/':
+                return query.path.split('/')[2]
+        # fail?
+        return None
+
+    def __extract_YT_transcript(self):
+        # assigning srt variable with the list
+        # of dictonaries obtained by the get_transcript() function
+        video_ID = self.__get_vid_id()
+
+        try:
+            srt = YouTubeTranscriptApi.get_transcript(video_ID)
+        except Exception:
+            srt = YouTubeTranscriptApi.get_transcript(video_ID, languages=['en-GB'])
+
+        full_text = ''
+        for text_item in srt:
+            text = text_item['text']
+            text_final = text + ' '
+            full_text = full_text + text_final
+
+        self.raw_text = full_text
+
+        # prints the result
+
+    def __extract_PDF_text(self):
+        final_string = ''
+        for page_layout in extract_pages(self.raw_media):
+            for element in page_layout:
+                if isinstance(element, LTTextContainer):
+                    final_string += element.get_text()
+
+        self.raw_text = final_string
+
 
     def clean_text(self):
         """
@@ -56,3 +120,4 @@ class Media:
         Once all steps are done, write the metadata for the media object to the database for performance & tracking.
         :return:
         """
+
--- Backend_build/Summarization_Models/LexRank_v_1/summarizer.py
+++ Backend_build/Summarization_Models/LexRank_v_1/summarizer.py
@@ -1,38 +1,55 @@
 from sumy.summarizers.lex_rank import LexRankSummarizer
+from nltk import sent_tokenize
+
 from sumy.nlp.tokenizers import Tokenizer
 import nltk
+from nltk import sent_tokenize
 nltk.data.path.append("/var/task/nltk_data")
 from sumy.parsers.plaintext import PlaintextParser
 import json
 
+
+def sentence_cleaner(sentence):
+    sentence = sentence.replace("\n", "")
+    sentence2 = " ".join(sentence.split())
+    sentence3 = sentence2.replace("-", "")
+    return sentence3
 
 def text_cleaner(full_text):
     """
     :param full_text: Full text to clean
     :return: Parsed & Cleaned text.
     """
-    clean_text = full_text.split(".")
+    #clean_text = full_text.split(".")
+    clean_text = sent_tokenize(full_text)
+    fully_cleaned_text = [sentence_cleaner(sentence) for sentence in clean_text]
+    #fully_cleaned_text2 = [" ".join(sent.split()) for sent in fully_cleaned_text]
+    #print(sentences[0])
+    #print(clean_text[0])
 
-    return clean_text
+    return fully_cleaned_text
 
 
 def summarization(full_text, reduce_length):
     clean_text = text_cleaner(full_text)
     full_text_length = len(clean_text)
 
+    print(clean_text)
+
     summary_perc = int(reduce_length)/100
 
     final_sentence_count = full_text_length * summary_perc
 
-    rawList = [item for item in full_text.splitlines() if item.strip()]
+    #rawList = [item for item in full_text.splitlines() if item.strip()]
 
-    rawList_str = ' '.join(rawList)
+    rawList_str = ' '.join(clean_text)
+    #print(rawList_str)
     parser = PlaintextParser.from_string(rawList_str, Tokenizer('english'))
     # Summarise
     Lex = LexRankSummarizer()
     Lex_summary = Lex(document=parser.document, sentences_count=round(final_sentence_count))
 
-    final_summary = ' '.join(str(v) for v in Lex_summary)
+    final_summary = '  '.join(str(v) for v in Lex_summary)
 
     return final_summary
 
