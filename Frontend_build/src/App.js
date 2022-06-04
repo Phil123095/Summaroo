1445e01d351e3ae06bd1d9565a36552ba2c2def0
Phil123095 <68194774+Phil123095@users.noreply.github.com>
2022-06-04 16:52:28 +0200

    Fixed background color

--- Frontend_build/src/App.js
+++ Frontend_build/src/App.js
@@ -3,7 +3,7 @@ import Router from "./Routes";
 
 function App() {
   return (
-      <div class="flex min-w-screen min-h-screen bg-slate-100">
+      <div class="flex min-w-screen min-h-screen bg-neutral-100">
         <Router/>
       </div>
   );
